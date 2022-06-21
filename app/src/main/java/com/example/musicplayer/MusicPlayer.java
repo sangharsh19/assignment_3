9d30d2f84d2ff90c50ccd4fa408c36a499267948
sangharsh19 <sangharshgurjar49@gmail.com>
2022-06-21 05:59:02 +0530

    git commit -m 'fisrt commit'

---
+++ app/src/main/java/com/example/musicplayer/MusicPlayer.java
@@ -0,0 +1,16 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPlayer extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
