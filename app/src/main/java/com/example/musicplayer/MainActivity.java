54c5743ce3ed2875b9722f90b2aba56e53edc343
sangharsh19 <sangharshgurjar49@gmail.com>
2022-06-21 06:00:54 +0530

    git commit -m 'second commit'

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button musicplay = (Button) findViewById(R.id.button);
+        musicplay.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
+    }
+}
